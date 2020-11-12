3008cac6921148ee3a1f018ff3b1d58f2930d85e
evgeny.konoplev <HorDex994>
2020-11-12 23:05:01 +0300

    frontend prorotype

--- frontend/pages/polls.vue
+++ frontend/pages/polls.vue
@@ -1,3 +1,57 @@
 <template>
-    <div><h1>Polls</h1></div>
+    <div class="container" v-if="$store.getters.getUser">
+
+        <h2 class="text-center mb-3">Опросы</h2>
+
+        <div class="jumbotron jumbotron-fluid py-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="border rounded p-3 bg-muted text-center">
+                            <base-icon name="pie-chart" :size="100"></base-icon>
+                        </div>
+                    </div>
+                    <div class="col-8">
+                        <dl class="row">
+                            <dt class="col-sm-3">Тема</dt>
+                            <dd class="col-sm-9">Команда "Team 1"</dd>
+                            <dt class="col-sm-3">Категория</dt>
+                            <dd class="col-sm-9">Результат спринта</dd>
+                            <dt class="col-sm-3">Дата</dt>
+                            <dd class="col-sm-9">August 15, 2020</dd>
+                        </dl>
+                        <p>Вы присутствовали на демонстрации функционала 11.11.2011 по результатам спринта. Пожалуйста, оцените результат работы.</p>
+                        <div class="row">
+                            <div class="col">
+                                <a href="#" class="btn btn-primary">Оценить</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col text-center">
+                <a href="#" class="btn btn-outline-secondary">
+                    <base-icon name="receipt" :size="16"></base-icon> Прежние опросы
+                </a>
+            </div>
+        </div>
+
+    </div>
 </template>
+
+<script>
+import BaseIcon from '../components/BaseIcon.vue';
+export default {
+    components: {
+        BaseIcon
+    },
+    head() {
+        return {
+            title: 'Опросы'
+        }
+    }
+}
+</script>
--- frontend/pages/work.vue
+++ frontend/pages/work.vue
@@ -11,13 +11,15 @@
                 
                 <dl class="row">
                     <dt class="col-sm-4">Текущая задача</dt>
-                    <dd class="col-sm-8">Анализ кода</dd>
+                    <dd class="col-sm-8">Анализ кода <a href="#"><base-icon name="list-task" :size="16"></base-icon></a></dd>
                     <dt class="col-sm-4">Выполнено задач за неделю</dt>
                     <dd class="col-sm-8">5</dd>
+                    <dt class="col-sm-4">На прошлой неделе</dt>
+                    <dd class="col-sm-8">15</dd>
                     <dt class="col-sm-4">Участие в команде</dt>
                     <dd class="col-sm-8">-</dd>
                     <dt class="col-sm-4">Оценка загруженности</dt>
-                    <dd class="col-sm-8">3 из 5</dd>
+                    <dd class="col-sm-8">3 из 5 <a href="#"><base-icon name="segmented-nav" :size="16"></base-icon></a></dd>
                     <dt class="col-sm-4">Последняя отслеживаемая активность</dt>
                     <dd class="col-sm-8">15 минут назад (коммит в репозиторий)</dd>
                     <dt class="col-sm-4">Коммитов в день</dt>
@@ -36,11 +38,15 @@
                 
                 <dl class="row">
                     <dt class="col-sm-4">Следующий отпуск</dt>
-                    <dd class="col-sm-8">через 3 месяца (01.01.21)</dd>
+                    <dd class="col-sm-8">через 3 месяца (01.01.21) 
+                        <a href="#"><base-icon name="calendar-check" :size="16"></base-icon></a>
+                    </dd>
                     <dt class="col-sm-4">Задач в очереди</dt>
-                    <dd class="col-sm-8">1</dd>
-                    <dt class="col-sm-4">Следующая встреча в каленаре</dt>
-                    <dd class="col-sm-8">10 ноября в 13:00</dd>
+                    <dd class="col-sm-8">1 <a href="#"><base-icon name="list-task" :size="16"></base-icon></a></dd>
+                    <dt class="col-sm-4">Следующая встреча в календаре</dt>
+                    <dd class="col-sm-8">10 ноября в 13:00
+                        <a href="#"><base-icon name="calendar-check" :size="16"></base-icon></a>
+                    </dd>
                     <dt class="col-sm-4">Достижение целей грейдирования</dt>
                     <dd class="col-sm-8">1 из 3</dd>
                     
@@ -48,6 +54,59 @@
             </div>
         </div>
 
+        <h2 class="text-center mb-3 mt-5">Командная занятость</h2>
+
+        <div class="card mt-4">
+            <div class="card-header">
+                Feature team <b>Team 1</b>
+            </div>
+            <div class="card-body">
+                
+                <dl class="row">
+                    <dt class="col-sm-4">Простаивает</dt>
+                    <dd class="col-sm-8">
+                        <a href="#"><base-icon name="person" :size="16"></base-icon> Петров Петр (дизайнер)</a>
+                    </dd>
+                    <dt class="col-sm-4">Задачи в спринте</dt>
+                    <dd class="col-sm-2">
+                        3 в очереди
+                    </dd>
+                    <dd class="col-sm-2">
+                        2 выполняются
+                    </dd>
+                    <dd class="col-sm-2">
+                        2 выполнено
+                    </dd>
+                    <dd class="col-sm-2">
+                        1 отменена <a href="#"><base-icon name="list-task" :size="16"></base-icon></a>
+                    </dd>
+                    <dt class="col-sm-4">Следующее собрание</dt>
+                    <dd class="col-sm-8">10 ноября в 13:00 - Утренний митинг
+                        <a href="#"><base-icon name="calendar-check" :size="16"></base-icon></a>
+                    </dd>
+                    <dt class="col-sm-4">Текущий спринт</dt>
+                    <dd class="col-sm-8">2</dd>
+                    <dt class="col-sm-4">Завершение</dt>
+                    <dd class="col-sm-8">14 ноября</dd>
+                </dl>
+            </div>
+        </div>
+
 
     </div>
 </template>
+
+
+<script>
+import BaseIcon from '../components/BaseIcon.vue';
+export default {
+    components: {
+        BaseIcon
+    },
+    head() {
+        return {
+            title: 'Занятость'
+        }
+    }
+}
+</script>
