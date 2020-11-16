3cec33cfb941dcbb42cbeea5193f6976810d8f71
effus <effusps@gmail.com>
2020-11-16 20:22:56 +0300

    issue 4

---
+++ frontend/components/BaseTabs.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul class="nav nav-tabs">
+        <li class="nav-item" v-for="item in tabs" :key="item.id">
+            <a class="nav-link" 
+                :href="'#' + item.id" 
+                :class="{'active': active && item.id === active.id}"
+                @click="onClickTab(item)"
+                >{{item.name}}</a>
+        </li>
+        <slot>
+
+        </slot>
+        
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        tabs: {
+            type: Array,
+            default: () => []
+        },
+        active: {
+            type: Object,
+            default: () => null
+        }
+    },
+    methods: {
+        onClickTab(item) {
+            this.$emit('tab-change', item);
+        }
+    }
+}
+</script>
